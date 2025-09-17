8576fc1dee4e117aba9afbeb6a7c5b076c1ebdc0
Muhammad Saad <muhammadsaadoo303@gmail.com>
2025-09-17 21:16:53 +0500

    first commit

---
+++ payments-core/src/main/java/com/interpay/payments/payments_core/PaymentsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.interpay.payments.payments_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsCoreApplication.class, args);
+	}
+
+}
---
+++ payments-core/src/test/java/com/interpay/payments/payments_core/PaymentsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interpay.payments.payments_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
