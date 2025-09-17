39d76fa5d930c3ab12cd4bb46c2b466a8486acc4
Muhammad Saad <muhammadsaadoo303@gmail.com>
2025-09-17 23:08:57 +0500

    docker is setting up

--- payments-core/src/main/java/com/interpay/payments/payments_core/PaymentsCoreApplication.java
+++ payments-core/src/main/java/com/interpay/payments/payments_core/PaymentsCoreApplication.java
@@ -2,12 +2,23 @@ package com.interpay.payments.payments_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class PaymentsCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentsCoreApplication.class, args);
+
+//		SpringApplication.run(PaymentsCoreApplication.class, args);
+
+		ConfigurableApplicationContext context=SpringApplication.run(PaymentsCoreApplication.class, args);
+
+		ConfigurableEnvironment environment=context.getEnvironment();
+		System.out.println(Arrays.toString(environment.getActiveProfiles()));
+		System.out.println("payments-core Service Running.......");
 	}
 
 }
