f1234a80fc3599250acf737152166a52cadfb542
Muhammad Saad <muhammadsaadoo303@gmail.com>
2025-09-22 23:15:24 +0500

    add rate limiter locally

--- user-service/src/main/java/com/fintech/user_service/configurations/SpringSecurityConfigurationsDocker.java
+++ user-service/src/main/java/com/fintech/user_service/configurations/SpringSecurityConfigurationsDocker.java
@@ -6,30 +6,18 @@ package com.fintech.user_service.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.client.ResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
-public class SpringSecurityConfigurationsDev {
+@Profile("docker")
+public class SpringSecurityConfigurationsDocker {
 
 //    @Autowired
 //    private JwtFilter jwtFilter;
