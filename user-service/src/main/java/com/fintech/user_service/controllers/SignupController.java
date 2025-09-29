02bc6f3b12a9be9505ddc0a54fd09ca6c06083e3
Muhammad Saad <muhammadsaadoo303@gmail.com>
2025-09-29 21:16:29 +0500

    milestone added

--- user-service/src/main/java/com/fintech/user_service/controllers/SignupController.java
+++ user-service/src/main/java/com/fintech/user_service/controllers/SignupController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/signup")
+@RequestMapping("/auth/signup")
 public class SignupController {
     @Autowired
     private SignupServiceImpl signupService;
--- user-service/src/main/java/com/fintech/user_service/services/SignupServiceImpl.java
+++ user-service/src/main/java/com/fintech/user_service/services/SignupServiceImpl.java
@@ -30,11 +30,10 @@ public class SignupServiceImpl {
     private EmailServiceImpl emailService;
 
 
+    private static final PasswordEncoder passwordencoder = new BCryptPasswordEncoder();
 
-    private static final PasswordEncoder passwordencoder=new BCryptPasswordEncoder();
 
-
-    private UserEntity user=null;
+    private UserEntity user = null;
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
@@ -47,7 +46,6 @@ public class SignupServiceImpl {
     private static final long EXPIRATION_TIME = 5;
 
 
-
     public ResponseEntity<?> verifyEmail(@Valid EmailVerificationRequest email_verification, BindingResult result) {
         // Check for validation errors
         try {
@@ -58,32 +56,31 @@ public class SignupServiceImpl {
                         .toList();
             }
 
-            Optional<UserEntity> user=authRepo.findByEmail(email_verification.getEmail());
-            if(user.isEmpty()){
+            Optional<UserEntity> user = authRepo.findByEmail(email_verification.getEmail());
+            if (user.isEmpty()) {
                 return ResponseEntity
                         .status(HttpStatus.NOT_FOUND)
-                        .body(email_verification.getEmail()+"    user not found");
+                        .body(email_verification.getEmail() + "    user not found");
             }
             //verify using code
             String storedCode = redisTemplate.opsForValue().get(email_verification.getEmail());
             if (storedCode != null && storedCode.equals(email_verification.getVerificationCode())) {
                 // If verified, remove the code from Redis
                 redisTemplate.delete(email_verification.getEmail());
-                UserEntity dbuser=user.get();
+                UserEntity dbuser = user.get();
 //                dbuser.setVerify(UserEntity.IsVerified.verified);
                 dbuser.setVerify(true);
-                UserEntity saveduser=authRepo.save(dbuser);
+                UserEntity saveduser = authRepo.save(dbuser);
 //                return ResponseEntity
 //                        .status(HttpStatus.CREATED)
 //                        .body(email_verification.getEmail()+"    verification successfull");
                 // Generate JWT with username and roles
-               jwtToken= jwtUtil.generateToken(saveduser.getEmail(), saveduser.getRole().toString());
+                jwtToken = jwtUtil.generateToken(saveduser.getEmail(), saveduser.getRole().toString());
                 return ResponseEntity.ok(new AuthResponse(jwtToken, dbuser));
             }
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("verification code is incorrect");
-
 
 
         } catch (Exception e) {
@@ -93,71 +90,21 @@ public class SignupServiceImpl {
                     .body("Exception occured");
         }
     }
+
     @Transactional
-    public ResponseEntity<?>insertUser(UserEntity user){
-
-
-
-            try {
-//                Optional<BanUserEntity> isban=banUserRepo.findByEmail(user.getEmail());
-//                if(isban.isPresent()){
-////                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();    main
-//                    return ResponseEntity
-//                            .status(HttpStatus.UNAUTHORIZED)
-//                            .body("the Email " + user.getEmail() + " is BAN");
-//                }
-                Optional<UserEntity> user_in_db=authRepo.findByEmail(user.getEmail());
-
-
-//                if(user_in_db.isPresent()){
-////                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();    main
-//                    return ResponseEntity
-//                            .status(HttpStatus.BAD_REQUEST)
-//                            .body( user.getEmail()+" already exist ");
-//                }
-                user.setPassword(passwordencoder.encode(user.getPassword()));
-                UserEntity is_add = authRepo.save(user);
-
-                // You can add custom logic if needed
-                if (is_add != null) {
-                    //save verification code in redis cloud
-                    Random random = new Random();
-                    String verificationCode = String.valueOf(1000 + random.nextInt(9000));
-                    System.out.println(verificationCode);
-                    if(emailService.sendEnail(user.getEmail(),"HairCare Ai","Your verification code is "+verificationCode)){
-
-                        try {//store verification code in redis cloud
-                            redisTemplate.opsForValue().set(user.getEmail(), verificationCode, EXPIRATION_TIME, TimeUnit.MINUTES);
-                            return ResponseEntity
-                                    .status(HttpStatus.CREATED)
-                                    .body( user.getEmail() +" registered successfully"+"  verification code is  "+  verificationCode);
-                        } catch (Exception e) {
-                            System.out.println(e);
-                            return ResponseEntity.internalServerError().build();
-                        }
-                    }
-                    return ResponseEntity.internalServerError().build();
-                } else {
-                    // Handle the case where the user could not be added
-                    return ResponseEntity.internalServerError().build();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                return ResponseEntity.internalServerError().build();
+    public ResponseEntity<?> insertUser(UserEntity user) {
+        try {
+            UserEntity saveUserInDb=authRepo.save(user);
+            if(saveUserInDb != null) {
+                return ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(user.getEmail() + " registered successfully");
             }
+            return ResponseEntity.internalServerError().build();
 
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
 
-
-    }
-//    public UserEntity insertUser(UserEntity user, MultipartFile imageFile) throws IOException {
-//        user.setImageName(imageFile.getOriginalFilename());
-//        user.setImageType(imageFile.getContentType());
-//        user.setImageData(imageFile.getBytes());
-//        user.setPassword(passwordencoder.encode(user.getPassword()));
 //
-//
-//        return authRepo.save(user);
-//
-//
-//    }
-}
+    }}
